c84e6527045d06f32b40de16a325ed999060817a
Awa Fall <awa.fall@hps-worldwide.com>
2026-07-21 12:05:27 +0100

    feat: Issuer Controller

--- src/main/java/com/cardsim/card_tokenization_simulator/dto/CardRequest.java
+++ src/main/java/com/cardsim/card_tokenization_simulator/dto/CardRequest.java
@@ -3,15 +3,17 @@ package com.cardsim.card_tokenization_simulator.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import java.time.LocalDate;
+
 public class CardRequest {
 
     @NotBlank(message = "PAN is required")
     @Pattern(regexp = "^[0-9]{13,19}$", message = "PAN must be 13-19 digits")
     private String pan;
 
-    @NotBlank(message = "Expiry date is required")
-    @Pattern(regexp = "^(0[1-9]|1[0-2])/([0-9]{2})$", message = "Expiry date must be in MM/YY format")
-    private String expiryDate;
+   /* @NotBlank(message = "Expiry date is required")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])/([0-9]{2})$", message = "Expiry date must be in MM/YY format")*/
+    private LocalDate expiryDate;
 
     @NotBlank(message = "Embossing name is required")
     private String embossingName;
@@ -24,11 +26,11 @@ public class CardRequest {
         this.pan = pan;
     }
 
-    public String getExpiryDate() {
+    public LocalDate getExpiryDate() {
         return expiryDate;
     }
 
-    public void setExpiryDate(String expiryDate) {
+    public void setExpiryDate(LocalDate expiryDate) {
         this.expiryDate = expiryDate;
     }
 
--- src/main/java/com/cardsim/card_tokenization_simulator/issuer/controller/IssuerController.java
+++ src/main/java/com/cardsim/card_tokenization_simulator/issuer/controller/IssuerController.java
@@ -1,4 +1,47 @@
 package com.cardsim.card_tokenization_simulator.issuer.controller;
 
+
+import com.cardsim.card_tokenization_simulator.issuer.dto.ApproveProvisioningRequest;
+import com.cardsim.card_tokenization_simulator.issuer.dto.ApproveProvisioningResponse;
+import com.cardsim.card_tokenization_simulator.issuer.dto.EligibilityRequest;
+import com.cardsim.card_tokenization_simulator.issuer.dto.EligibilityResponse;
+import com.cardsim.card_tokenization_simulator.issuer.service.IssuerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/issuer")
 public class IssuerController {
+
+    private final IssuerService issuerService;
+    public IssuerController(IssuerService issuerService) {
+        this.issuerService = issuerService;
+    }
+
+
+    @PostMapping("/eligibility")
+    public ResponseEntity<EligibilityResponse> checkElligibilty(
+            @Valid @RequestBody EligibilityRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(issuerService.checkEligibility(request));
+    }
+
+
+    @PostMapping("/provisioning/approve")
+    public ResponseEntity<ApproveProvisioningResponse> approveProvisioning(
+            @Valid @RequestBody ApproveProvisioningRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(issuerService.approveProvisioning(request));
+    }
+
+
+
 }
+
